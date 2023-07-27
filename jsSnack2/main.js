e81bc51722068c1a75af8c90ba695e59db5abb86
Ericdid <ericdidom@gmail.com>
2023-07-27 17:09:46 +0200

    add.snack 2

--- jsSnack2/main.js
+++ jsSnack2/main.js
@@ -5,8 +5,29 @@ const nameList = ["marco", "paolo", "geeno", "mario"];
 
 const surnameList = ["maria mario", "sciamenna", "fernandello", "fuffolo"];
 
-// prendiamo un nome a caso
+// generiamo lista vuota di invitati
 
-for (let i = 0; i < nameList.length; i++) {}
+const invites = [];
 
-console.log(nameList);
+// chiediamo all utente il numero di invitati
+
+const invNumb = parseInt(prompt("quanta gente invitare?"));
+
+// generiamo invitati
+
+for (let i = 0; i < invNumb; i++) {
+  //prendo un nome casuale dalla lista
+  const randomInvName = Math.floor(Math.random() * nameList.length);
+  //nome recuperato
+  const randomname = nameList[randomInvName];
+  //prendo un cognome casuale dalla lista
+  const randomInvSurname = Math.floor(Math.random() * surnameList.length);
+  //cognome recuperato
+  const randomSurname = surnameList[randomInvSurname];
+  // metto insieme nome casuale e cognome casuale
+  const fakeGuest = randomname + " " + randomSurname;
+  // pusho nella lista invitati
+  invites.push(fakeGuest);
+}
+
+console.table(invites);
