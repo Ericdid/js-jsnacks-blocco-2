93f52efc7727c6bc3089fa6792f5ec036b7b5607
Ericdid <ericdidom@gmail.com>
2023-07-27 16:45:56 +0200

    add.snack-1

---
+++ jsSnack2/main.js
@@ -0,0 +1,12 @@
+// generiamo una lista di Nomi
+const nameList = ["marco", "paolo", "geeno", "mario"];
+
+// generiamo una lista di Cognomi
+
+const surnameList = ["maria mario", "sciamenna", "fernandello", "fuffolo"];
+
+// prendiamo un nome a caso
+
+for (let i = 0; i < nameList.length; i++) {}
+
+console.log(nameList);
